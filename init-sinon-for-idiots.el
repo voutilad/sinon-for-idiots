20452e9e74f6422a3fa4674c9c4e2e097e8421ed
Dave Voutila <voutilad@gmail.com>
2018-03-21 12:50:01 -0400

    draft in progress

---
+++ init-sinon-for-idiots.el
@@ -0,0 +1,16 @@
+;;; init-sinon-for-idiots.el --- Initialize our Presentation
+;;
+;; Copyright (C) 2018 Dave Voutila
+;;
+;; Author: Dave Voutila
+;;
+;;; Commentary:
+;; sets up "org-present" mode
+;;
+;;; Code:
+
+(add-to-list 'load-path "./org-present")
+(autoload 'org-present "org-present" nil t)
+
+(provide 'init-sinon-for-idiots)
+;;; init-sinon-for-idiots.el ends here
